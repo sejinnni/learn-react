7a819580f08cdcd1943c0f112554d427d351bfed
전세진 <jeonsejin@jeonsejin-ui-MacBookAir.local>
2025-08-29 16:58:30 +0900

    카운터 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import {useState} from 'react'
+
+const Counter = () => {
+  const [number,setNumber] = useState(0);
+  const handleClick=()=>{
+    setNumber(number+1);
+  }
+  return(
+    <div>
+      <button onClick={handleClick}>number :{number} </button>
+    </div>
+  );
+}
 
 const App = () => {
 
   return (
     <>
-     
+       <Counter/>
     </>
   )
 }
