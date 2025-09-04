1bfb79a2814d8dd72f9ca28166730dad2c7eaeb9
전세진 <jeonsejin@jeonsejin-ui-MacBookAir.local>
2025-09-04 13:25:45 +0900

    좋아요버튼

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import './App.css'
 import {useState} from 'react'
 
-function TextInput(){
-  const [text,setText] =useState("");
-  const handle = (e)=>{
-    setText(e.target.value);
+function LikeButton(){
+  const colors = ["red","gray"];
+  const [count,setCount] = useState(0);
+  const [colorArrange,setColorArrange] = useState(colors[0]);
+
+  const likeHandle =()=>{
+    setCount(count+1);
+    setColorArrange(colors[(count+1) % colors.length]);
   }
-  
   return(
-    <form>
-      <input type="text" value={text} onChange={handle}/>
-      <p>입력한 텍스트: {text}</p>
-    </form>
+    <>
+      <button onClick={likeHandle} style={{backgroundColor: colorArrange}} ></button>
+      <p>좋아요수 : {count}</p>
+    </>
   )
 }
-
 const App = () => {
   return (
-    <TextInput/>
+    <LikeButton/>
   )
 }
 
