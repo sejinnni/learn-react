9d678991ffb69d399cf1b286977cd07971cae891
전세진 <jeonsejin@jeonsejin-ui-MacBookAir.local>
2025-09-05 18:29:25 +0900

    탭전환

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 import './App.css'
 import {useState} from 'react'
 
-function LikeButton(){
-  const colors = ["red","gray"];
-  const [count,setCount] = useState(0);
-  const [colorArrange,setColorArrange] = useState(colors[0]);
-
-  const likeHandle =()=>{
-    setCount(count+1);
-    setColorArrange(colors[(count+1) % colors.length]);
+function TapButton(){
+  const [content,setConent]=useState(0);
+  const homeHandle=()=>{
+    setConent(1);
   }
+  const productHandle=()=>{
+    setConent(2);
+  }
+  const callHandle=()=>{
+    setConent(3);
+  }
+
   return(
     <>
-      <button onClick={likeHandle} style={{backgroundColor: colorArrange}} ></button>
-      <p>좋아요수 : {count}</p>
+      <div>
+        <button onClick={homeHandle}>홈</button>
+        <button onClick={productHandle}>상품</button>
+        <button onClick={callHandle}>고객문의</button>
+      </div>
+      <div>
+        {content === 1 && <p>홈 화면입니다.</p>}
+        {content === 2 && <p>제품 상세화면입니다.</p>}
+        {content === 3 && <p> 문의 화면입니다.</p>}
+      </div>
+
     </>
   )
 }
 const App = () => {
   return (
-    <LikeButton/>
+    <TapButton/>
   )
 }
 
