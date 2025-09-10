d17717fbcd82a7d2eb54b50c2038ebb2e2b334e6
전세진 <jeonsejin@jeonsejin-ui-MacBookAir.local>
2025-09-10 10:36:05 +0900

    메시지뱃지

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,24 @@
 import './App.css'
 import {useState} from 'react'
 
-function TodoList(){
-  const [todoList, setTodoList] = useState(["리액트 문제풀기", "포트폴리오 만들기"]);
-  const [input, setInput] = useState("");
-
-  const handleInputChange = (e) => {
-    setInput(e.target.value);
-  }
-
-  const addTodo = () => {
-    if (input.trim() !== "") {
-      setTodoList([...todoList, input]);
-      setInput(""); // 입력 필드 초기화
-    }
-  }
+function Message({count}){
 
   return(
-    <>
-    <input 
-      type="text" 
-      value={input} 
-      onChange={handleInputChange}
-      placeholder="할 일을 입력하세요"
-    />
-    <ul>
-      {todoList.map((todo, index) => (
-        <li key={index}>{todo}</li>
-      ))}
-    </ul>
-    <button onClick={addTodo}>추가</button>
-    </>
+      <div style={{display:"flex",gap:"20px"}}>
+        메시지<div style={{ backgroundColor:"red", padding:"0 12px", borderRadius:"4px"}}>{count === 0 ? 0 : count > 99 ? `99+` : count}</div>
+      </div>
   )
 }
 
 const App = () => {
   return (
-    <TodoList/>
+   <div style={{display:"flex",gap:"20px"}}>
+    <Message count={0}/>
+    <Message count={5}/>
+    <Message count={100}/>
+   </div> 
+    
+
   )
 }
 
