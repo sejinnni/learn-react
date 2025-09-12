cbbdbda6d933157486e4008159fe81b673559869
전세진 <jeonsejin@jeonsejin-ui-MacBookAir.local>
2025-09-12 10:58:07 +0900

    리스트렌더링1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import './App.css'
 import {useState} from 'react'
 
-function Button({status, style}){
+const fruits =["사과","바나나","오렌지","포도"];
+
+function FruitList({fruits}){
   return(
-    <>
-    <button style={style}>
-      { status > 0 ? "구매하기" : status < 0 ? "품절" : "출시예정" } </button>
-    </>
+    <div>
+      {fruits.map((fruit,index)=>(
+        <ul key={index}>
+          <li>{fruit}</li>
+        </ul>
+      ))}
+    </div>
   )
 }
 
 const App = () => {
   return (
-    <div style={{display:"flex", gap:"20px"}}>
-      <Button status={1} style={{backgroundColor:"#007bff"}}/>
-      <Button status={-1} style={{backgroundColor:"#6c757d"}} />
-      <Button status={0} style={{backgroundColor:"#6f42c1"}}/>
-    </div>
+    <FruitList fruits={fruits}/>
   )
 }
 
