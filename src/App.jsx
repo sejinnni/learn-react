6fd1389a3fd36c9f2a2ae88b32095103b6c7b176
jinnie <zlchlrh@gmail.com>
2025-09-18 10:15:03 +0900

    리스트렌더링2

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,28 @@
 import './App.css'
-import {useState} from 'react'
-import {useEffect} from "react";
 
- function Timer(){
-  const [count,setCount] = useState(10);
-  const [isRun, setIsRun]= useState(false);
+const members = [
+  {id: "1", name: "김호준", job: "디자이너"},
+  {id: "2", name: "이민수", job: "개발자"},
+  {id: "3", name: "박지영", job: "PM"}
+];
 
-  useEffect(()=>{
-    console.log(isRun);
-    if(!isRun) return;
 
-    const Interval = setInterval(()=>{
-      setCount(prevCount => {
-        if (prevCount <= 1) {
-          setIsRun(false);
-          return 0;
-        }
-        return prevCount - 1;
-      });
-    },1000)
-
-    return()=>clearInterval(Interval);
-  },[isRun]);
-  
-  const handle=()=>{
-    console.log('시작버튼');
-    setIsRun(true);
-  }
-
-  return(
-    <>
-      <h2>{count}</h2>
-      <button onClick={handle}>시작</button>
-    </>
+function CardList({members}){
+  return (
+    <div style={{display:"flex",gap:"40px"}}>
+      {members.map((person)=>(
+        <div key={person.id} style={{border : "1px solid gray", padding:"12px 60px", borderRadius:"8px"}}>
+          <p>{person.name}</p>
+          <p>{person.job}</p>
+        </div>
+      ))}
+    </div>
   )
- }
-
-
+}
 
 const App = () => {
   return (
-    <Timer/>
+    <CardList members={members}/>
   )
 }
 
