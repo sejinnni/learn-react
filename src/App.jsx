7a0e67fd866eada50c35af14937eb25308f3d5b1
jinnie <zlchlrh@gmail.com>
2025-09-25 13:48:36 +0900

    쇼핑카트

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
 import './App.css'
 import {useState} from 'react'
 
+function ProductList(){
+  const [state,setState] = useState([]);
 
-function ToggleButton(){
-  const [state,setState] = useState(true);
-  const handle=()=>{
-    setState(!state)
+  const products = [
+    {id : 1, name :"사과", price :"1000"},
+    {id : 2, name :"바나나", price :"2000"},
+    {id : 3, name :"딸기", price :"3000"},
+  ]
+
+  const addCart = (product)=>{
+    setState([...state,product]);
   }
 
-  return(
-  <div>
-    <button onClick={handle}>
-      {state ?"Light" : "Dark"}
-    </button>
-
-    {state && <p style={{backgroundColor:"white", color:"black", padding:"12px 24px"}}>라이트 모드입니다.</p>}
-    {!state && <p style={{backgroundColor:"black", color:"white", padding:"12px 24px"}}>다크 모드입니다.</p>}
-  </div>
-
+  const deleteCart = (id)=>{
+    setState(state.filter((product)=> product.id !== id))
+  }
+  return (
+    <>
+      <div>
+        <h2>상품목록</h2>
+        <button onClick={addCart}>추가버튼</button>
+      </div>
+      <div>
+        <h2>장바구니</h2>
+        <button onClick={deleteCart}>제거버튼</button>
+      </div>
+      <p>총액 : </p>
+    </>
   )
 }
 
 const App = () => {
   return (
-    <>  
-    <ToggleButton/>
+    <> 
+      <ProductList/>
     </>
 
   )
